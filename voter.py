2e7f4b70c6f84946d543718f989c6d780b0504be
benpopisith <bpopisith@dons.usfca.edu>
2026-06-15 20:17:43 -0700

    implement ranked choice voting

--- voter.py
+++ voter.py
@@ -3,10 +3,21 @@ class Voter:
     def __init__(self, id, leaning):
         self.id = id
         self.leaning = leaning
+        self.ranked_votes = []
 
     def __repr__(self):
         return (
             f"Voter with ID={self.id}, "
             f"leaning: {self.leaning:.3f}"
         )
+    
+    def rank_candidates(self, candidates):
+        ranked_candidates = []
+        for candidate in candidates:
+            abs_dist = abs(self.leaning - candidate.leaning)
+            candidate_tuple = (abs_dist, candidate)
+            ranked_candidates.append(candidate_tuple)
+        self.ranked_votes = sorted(ranked_candidates)
+        
+
 
--- voting_system.py
+++ voting_system.py
@@ -8,6 +8,7 @@ class VotingSystem:
     def __init__(self):
         self.candidates = []
         self.voters = []
+        self.eliminated_candidates = []
 
     def generate_candidates(self, count):
         names = ['Aang', 'Katara', 'Sokka', 'Zuko', 'Iroh', 'Appa', 'Momo', 'Toph', 'Azula', 'Suki', 'Ozai', 'Mai', 'Ty']
@@ -27,6 +28,41 @@ class VotingSystem:
             )
             for i in range(count)
         ]
+    
+    def generate_winner(self):
+        # call rank_candidates for every voter
+        for voter in self.voters:
+            voter.rank_candidates(self.candidates)
+        
+        has_winner = False
+        self.eliminated_candidates = []
+        winner = ''
+        round_count = 0
+
+        while not has_winner:
+            round_count += 1
+            vote_count = {}
+            print(f'Round {round_count}!')
+            for voter in self.voters:
+                for ranked_vote in voter.ranked_votes:
+                    candidate = ranked_vote[1]
+                    if candidate not in self.eliminated_candidates:
+                        vote_count[candidate] = vote_count.get(candidate, 0) + 1
+                        break
+            
+            for candidate ,votes in vote_count.items():
+                percent_of_votes = 100 * (votes / (len(self.voters)))
+                print(f"{candidate}, {percent_of_votes:.2f}%")
+                if votes > .5 * (len(self.voters)):
+                    has_winner = True
+                    winner = candidate
+            if not has_winner:
+                least_voted = min(vote_count, key=vote_count.get)
+                self.eliminated_candidates.append(least_voted)
+        
+        print(f'The winner is: {winner.name}')
+
+            
 
 if __name__ == "__main__":
     voting_system = VotingSystem()
@@ -41,3 +77,5 @@ if __name__ == "__main__":
     print("\nVoters:")
     for voter in voting_system.voters:
         print(voter)
+
+    voting_system.generate_winner()
